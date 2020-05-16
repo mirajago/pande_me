486f0cf2832ffb10a294ddd95646babdfce60a16
tony-albanese <tony.albanese2000@gmail.com>
2020-05-16 13:38:35 +0200

    Initial commit. Copied from project.

---
+++ lib/utility_methods/google_sign_in_methods.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
+final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+String name;
+String email;
+String imageUrl;
+
+void signOutGoogle() async {
+  await googleSignIn.signOut();
+}
+
+void signOutFirebase() async {
+  await firebaseAuth.signOut();
+}
+
+Future<String> signIn() async {
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.getCredential(
+      idToken: googleSignInAuthentication.idToken,
+      accessToken: googleSignInAuthentication.accessToken);
+
+  final AuthResult authResult =
+      await firebaseAuth.signInWithCredential(credential);
+  final FirebaseUser firebaseUser = authResult.user;
+
+  name = firebaseUser.displayName;
+  email = firebaseUser.email;
+  imageUrl = firebaseUser.photoUrl;
+
+  // Only taking the first part of the name, i.e., First Name
+  if (name.contains(" ")) {
+    name = name.substring(0, name.indexOf(" "));
+  }
+
+  final FirebaseUser currentUser = await firebaseAuth.currentUser();
+  assert(firebaseUser.uid == currentUser.uid);
+
+  return 'signInWithGoogle succeeded: $firebaseUser';
+}
