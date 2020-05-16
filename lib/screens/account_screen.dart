3ee79cc3d152e060235790cb9a0bcc38c19d683c
Jonas <jonas.baldowe1@web.de>
2020-05-16 16:11:26 +0200

    login handling done

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pandeme/services/login_handler.dart';
 import 'package:pandeme/widgets/appbar_widget.dart';
 import 'package:pandeme/helpers/colors.dart' as colors;
 
@@ -8,21 +9,89 @@ class AccountScreen extends StatefulWidget {
 }
 
 class _AccountScreenState extends State<AccountScreen> {
+  bool _registerMode = true;
+  void _register() {
+    if (pwController.text == rpwController.text) {
+      registerWithEmailAndPassword(mailController.text, pwController.text);
+    }
+  }
+
+  void _signIn() {
+    signIn(mailController.text, pwController.text)
+        .then((user) => print(user.mail + " logged in"));
+  }
+
+  void switchToSignIn() {}
+
+  final mailController = TextEditingController();
+  final pwController = TextEditingController();
+  final rpwController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppbarWidget(),
       body: Container(
-          width: MediaQuery.of(context).size.width,
-          color: colors.black,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                  color: colors.purpurRot,
-                  child: Text("Account")),
-            ],
-          )
+        width: MediaQuery.of(context).size.width,
+        color: colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _registerMode ? Text("Registering") : Text("Sign In"),
+            TextField(
+              controller: mailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            TextField(
+              controller: pwController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            _registerMode
+                ? Column(children: <Widget>[
+                    TextField(
+                      controller: pwController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Repeat Password',
+                      ),
+                    ),
+                    RaisedButton(
+                      onPressed: _register,
+                      child: Text("Register"),
+                    ),
+                    FlatButton(
+                        child: Text("Already have an account?"),
+                        onPressed: () {
+                          setState(() {
+                            _registerMode = false;
+                          });
+                        })
+                  ])
+                : Column(
+                    children: <Widget>[
+                      RaisedButton(
+                        onPressed: _signIn,
+                        child: Text("Sign In"),
+                      ),
+                      FlatButton(
+                        onPressed: () {
+                          setState(() {
+                            _registerMode = true;
+                          });
+                        },
+                        child: Text(
+                          ("Don't have an account?"),
+                        ),
+                      ),
+                    ],
+                  ),
+          ],
+        ),
       ),
     );
   }
--- lib/services/login_handler.dart
+++ lib/services/login_handler.dart
@@ -1,69 +1,47 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:pandeme/model/user.dart';
 
 final GoogleSignIn _googleSignIn = GoogleSignIn();
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
-Future registerWithEmailAndPassword(String mail, String password) async {
+Future<User> signIn(String mail, String password) async {
+  try {
+    AuthResult result =
+        await _auth.signInWithEmailAndPassword(email: mail, password: password);
+    FirebaseUser user = result.user;
+    return (_getUserfromFirebaseUser(user));
+  } catch (e) {
+    print(e.toString());
+    return null;
+  }
+}
+
+Future<User> registerWithEmailAndPassword(String mail, String password) async {
   try {
     AuthResult result = await _auth.createUserWithEmailAndPassword(
         email: mail, password: password);
     FirebaseUser user = result.user;
-  } catch (e) {}
+    return (_getUserfromFirebaseUser(user));
+  } catch (e) {
+    print(e.toString());
+    return null;
+  }
 }
 
-Future<FirebaseUser> handleSignIn() async {
-  final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-  final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-
-  final AuthCredential credential = GoogleAuthProvider.getCredential(
-    accessToken: googleAuth.accessToken,
-    idToken: googleAuth.idToken,
-  );
-
-  final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
-  print("signed in " + user.displayName);
-  return user;
+User _getUserfromFirebaseUser(FirebaseUser user) {
+  return user != null ? User(user.uid, user.email) : null;
 }
 
-// final GoogleSignIn googleSignIn = GoogleSignIn();
-// final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-
-// String name;
-// String email;
-// String imageUrl;
-
-// void signOutGoogle() async {
-//   await googleSignIn.signOut();
-// }
-
-// void signOutFirebase() async {
-//   await firebaseAuth.signOut();
-// }
-
-// Future<String> signIn() async {
-//   final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-//   final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+//   final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+//   final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
 
 //   final AuthCredential credential = GoogleAuthProvider.getCredential(
-//       idToken: googleSignInAuthentication.idToken,
-//       accessToken: googleSignInAuthentication.accessToken);
+//     accessToken: googleAuth.accessToken,
+//     idToken: googleAuth.idToken,
+//   );
 
-//   final AuthResult authResult = await firebaseAuth.signInWithCredential(credential);
-//   final FirebaseUser firebaseUser = authResult.user;
-
-//   name = firebaseUser.displayName;
-//   email = firebaseUser.email;
-//   imageUrl = firebaseUser.photoUrl;
-
-//   // Only taking the first part of the name, i.e., First Name
-//   if (name.contains(" ")) {
-//     name = name.substring(0, name.indexOf(" "));
-//   }
-
-//   final FirebaseUser currentUser = await firebaseAuth.currentUser();
-//   assert(firebaseUser.uid == currentUser.uid);
-
-//   return 'signInWithGoogle succeeded: $firebaseUser';
-
+//   final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+//   print("signed in " + user.displayName);
+//   return user;
 // }
