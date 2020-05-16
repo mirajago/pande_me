c784778f4f18fce02debc3bd1a5a1a02d7c710ec
tony-albanese <tony.albanese2000@gmail.com>
2020-05-16 13:35:52 +0200

    Added import for Google sign in button widget.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pandeme/widgets/button_google_sign_in.dart';
 
 
 class LoginPageScaffold extends StatelessWidget {
@@ -32,28 +33,3 @@ class LoginPageColumnWidget extends StatelessWidget {
   }
 }
 
-class GoogleLoginButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-      child: Text("Login With Google"),
-      color: Colors.blue,
-      textColor: Colors.white,
-      elevation: 2,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10)
-      ),
-      onPressed: (){
-        print("GoogleLoginButton pressed.");
-
-        signIn().whenComplete((){
-          Navigator.of(context).push(MaterialPageRoute(
-              builder: (context){
-                //TODO: Return route for when the user has successfully signed in.
-              }
-          ));
-        });
-      },
-    );
-  }
-}
---
+++ lib/widgets/button_google_sign_in.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class GoogleLoginButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      child: Text("Login With Google"),
+      color: Colors.blue,
+      textColor: Colors.white,
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10)
+      ),
+      onPressed: () {
+        print("GoogleLoginButton pressed.");
+
+        signIn().whenComplete(() {
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) {
+                //TODO: Return the page for  when the user signed in.
+              }
+          ));
+        });
+      },
+    );
+  }
